7962fc22259c108c0c18b8f284e9541c7c96b923
AfterEmerald <afteremerald@gmail.com>
2015-07-01 20:37:21 -0400

    Navbar stuff

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,12 @@
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-fixed-top").addClass("scrollednav");
+        $(".navbar-fixed-top").removeClass("topnav");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-fixed-top").removeClass("scrollednav");
+        $(".navbar-fixed-top").addClass("topnav");
     }
 });
 
